84fa6ba8fa7421049cdc4256995b11128139348c
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-04-10 17:23:10 +0530

    aded react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='italic'>Hello</h1>
+      <h1 className='italic '>Hello</h1>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+import Router from './routes/index.jsx';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
