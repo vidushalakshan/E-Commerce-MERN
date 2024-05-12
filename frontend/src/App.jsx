1416eeee7be3463c42726e76b242e7e9f769d47b
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-05-12 13:01:28 +0530

    aded logo and update some content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
@@ -6,7 +7,10 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <main>
+        <Home /> 
+      </main>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { logo } from '../assest';
 
 const Header = () => {
   return (
-    <div>
-        <nav>
-            logo
-        </nav>
-    </div>
+    <header className='h-16 shadow-md'>
+      <div className='container flex items-center h-full px-4 mx-auto'>
+        <div>
+            <img src={logo} alt="Nav Logo" className='w-[135px] h-[90px]'/>
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
