d202c93657fa8c87b27d86ba2df9766e2387093b
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-05-20 19:06:25 +0530

    aded user loging and add cart section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { logo } from "../assest";
 import { HiOutlineSearch } from "react-icons/hi";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -11,13 +13,29 @@ const Header = () => {
         </div>
 
         <div className="hidden w-full max-w-sm pl-2 border rounded-full lg:flex justinfy-between items-starts-center focus-within:shadow">
-          <input type="text" placeholder="search product here...."  className="w-full outline-none"/>
-          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-full">
+          <input
+            type="text"
+            placeholder="search product here...."
+            className="w-full outline-none"
+          />
+          <div className="text-lg min-w-[50px] h-8 text-white bg-red-600 flex items-center justify-center rounded-full">
             <HiOutlineSearch />
           </div>
         </div>
 
-        <div>user icons and card</div>
+        <div className="flex items-center justify-center gap-4">
+          <div className="text-3xl cursor-pointer">
+            <FaRegCircleUser />
+          </div>
+
+          <div className="relative text-2xl cursor-pointer">
+            <span>
+              <FaShoppingCart />
+            </span>
+            
+            <p className="absolute flex items-center justify-center w-5 h-5 text-sm text-white bg-red-600 rounded-full bottom-3 left-4">0</p>
+          </div>
+        </div>
       </div>
     </header>
   );
