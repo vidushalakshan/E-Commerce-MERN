8f7b584e9abbacaf15fff09a712b711455d17dee
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-05-24 12:56:55 +0530

    aded login page sign up link and styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { loginIcon } from "../assest";
-import { FaEye } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div id="login">
       <div className="container p-4 mx-auto">
-        <div className="max-w-sm p-2 py-5 mx-auto bg-white">
+        <div className="max-w-sm p-4 mx-auto bg-white">
+          {/* login image icon  */}
           <div className="w-20 h-20 mx-auto">
             <img src={loginIcon} alt="login-icon" />
           </div>
+          {/* />login image icon  */}
 
+          {/* start form section */}
           <form action="" className="mt-6">
+            {/* email text section */}
             <div className="">
               <label htmlFor="">Email :</label>
               <div className="p-2 bg-slate-100">
@@ -23,6 +26,9 @@ const Login = () => {
                 />
               </div>
             </div>
+            {/* /> email text section */}
+
+            {/* password text section */}
             <div>
               <label htmlFor="">Password :</label>
               <div className="flex p-2 bg-slate-100">
@@ -32,16 +38,29 @@ const Login = () => {
                   className="w-full h-full bg-transparent outline-none"
                 />
               </div>
+              {/*/> password text section */}
 
-              <Link to={'/fogot-password'} className="text-[14px] ml-auto block w-fit underline hover:text-red-600  ">
+              {/* fogot password link  */}
+              <Link
+                to={"/fogot-password"}
+                className="text-[14px] ml-auto block w-fit underline hover:text-red-600  "
+              >
                 Forgot Password
               </Link>
             </div>
+            {/* /> fogot password link  */}
 
-            <button
-              className="block px-6 py-2 mx-auto mt-4 text-white transition-all bg-red-500 rounded hover:scale-110"           
-            >Login</button>
+            {/* login button */}
+            <button className="block px-6 py-2 mx-auto mt-4 text-white transition-all bg-red-500 rounded hover:scale-110">
+              Login
+            </button>
+             {/* /> login button */}
           </form>
+          {/* />start form section */}
+
+          {/* singup link */}
+            <p className="my-5">Don't have account ? <Link to={'/sign-up'} className="text-red-500 hover:text-red-600 hover:underline">Sign up</Link></p>
+           {/* /> singup link */}
         </div>
       </div>
     </div>
