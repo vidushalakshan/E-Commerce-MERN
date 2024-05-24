f8014fb66935406b470b867e281aa24fb74e53a1
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-05-24 12:41:24 +0530

    update fogot-password logo icon middle

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -3,10 +3,10 @@ import { fogotPassword } from '../assest';
 
 const ForgotPassword = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <div>
+    <div id='fogot-password'>
+        <div className='container p-4 mx-auto'>
+            <div className='max-w-md p-2 py-5 mx-auto bg-white'>
+                <div className='w-20 h-20 mx-auto '>
                     <img src={fogotPassword} alt="fogot-password"/>
                 </div>
             </div>
