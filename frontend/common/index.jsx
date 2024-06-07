60b0b0510317898201aa14ca9937d3f306b46720
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-06-07 17:51:25 +0530

    create backend domain

---
+++ frontend/common/index.jsx
@@ -0,0 +1,10 @@
+const backendDomain = "http://localhost:8080"
+
+const SummaryApi = {
+    SignUp: {
+        url :`${backendDomain}/api/singup`,
+        method : "post"
+    }
+}
+
+export default SummaryApi;
