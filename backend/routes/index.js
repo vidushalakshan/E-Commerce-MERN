368baaaee3b985b678b6e819ae6f57747815aa11
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-06-08 12:28:48 +0530

    updated userDetails and authToken Route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,11 @@ const router = express.Router()
 
 const userSignupController = require("../controller/userSingup")
 const userSingInController = require('../controller/userSignin')
+const userDetailsController = require('../controller/userDetails')
+const authToken = require('../middleware/authToken')
 
 router.post("/singup",userSignupController)
 router.post("/login",userSingInController)
+router.get("/user-details",authToken, userDetailsController)
 
 module.exports = router;
