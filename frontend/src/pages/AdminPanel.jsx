79c828b396d06d08742d014dcb83d341934c00eb
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-06-11 18:37:15 +0530

    updated admin panel some styling

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const AdminPanel = () => {
 
     const user = useSelector((state) => state.user.user);
     
   return (
-    <div className="min-h[calc(100vh-120px)] flex">
-      <aside className="w-full min-h-full bg-white max-w-60 customShadow">
-        <div className="flex flex-col items-center justify-center h-32">
+    <div className='min-h-[calc(100vh-120px)] md:flex hidden'>
+      <aside className='w-full min-h-full bg-white max-w-60 customShadow'>
+        <div className='flex flex-col items-center justify-center h-32'>
           <div
-            className="relative flex justify-center text-5xl cursor-pointer"
+            className='relative flex justify-center text-5xl cursor-pointer'
           >
             {user?.profilepic ? (
               <img
@@ -24,6 +25,14 @@ const AdminPanel = () => {
             )}
           </div>
           <p className="text-lg font-semibold capitalize">{user?.name}Vidusha Lakshan</p>
+        </div>
+
+            {/* navigation */}
+        <div>
+            <nav className="grid p-4">
+                <Link to={"all-users"}className="px-2 py-1 hover:bg-slate-100">All Users</Link>
+                <Link to={"product"}className="px-2 py-1 hover:bg-slate-100">Product</Link>
+            </nav>
         </div>
       </aside>
             
