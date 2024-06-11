fcd0cfd9304c27040ef77181b03686042b492131
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-06-11 15:22:06 +0530

    aded admin panel button in header section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { logo } from "../assest";
 import { HiOutlineSearch } from "react-icons/hi";
 import { FaRegCircleUser } from "react-icons/fa6";
@@ -12,7 +12,7 @@ import { clearUserDetails } from "../store/userSlice";
 const Header = () => {
   const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
-  console.log("user model", user);
+  const [menuDisplay, setMenuDisplay] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -52,15 +52,30 @@ const Header = () => {
         </div>
 
         <div className="flex items-center justify-center gap-7">
-          <div className="text-3xl cursor-pointer">
-            {user?.profilepic ? (
-              <img
-                src={user.profilepic}
-                alt="User Profile"
-                className="w-10 h-10 rounded-full"
-              />
-            ) : (
-              <FaRegCircleUser />
+          <div className="relative flex justify-center ">
+            <div className="text-3xl cursor-pointer" onClick={() => setMenuDisplay(preve => !preve)}>
+              {user?.profilepic ? (
+                <img
+                  src={user.profilepic}
+                  alt="User Profile"
+                  className="w-10 h-10 rounded-full"
+                />
+              ) : (
+                <FaRegCircleUser />
+              )}
+            </div>
+
+            {menuDisplay && (
+              <div className="absolute bottom-0 p-2 bg-white rounded shadow-lg top-11 h-fit ">
+                <nav>
+                  <Link
+                    to={"admin-panel"}
+                    className="p-2 whitespace-nowrap hover:bg-slate-100"
+                  >
+                    Admin Panel
+                  </Link>
+                </nav>
+              </div>
             )}
           </div>
 
